808b0817bd26795f8bc2f6e629939064ee1bef13
lugh-kohei <ajqmjpdtpmt@gmail.com>
2025-07-29 09:49:53 +0900

    認証機能作成

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('signup/', views.RegisterView.as_view(), name='user-signup'), # 新規登録処理'
-    path('login/', views.LoginView.as_view(), name='user-login'), # ログイン'
-    path('getuser/', views.GetProfileView.as_view(), name='get-userprofile'), #プロフィール取得
-    path('gettoken/', views.GetTokenView.as_view(), name='get-token'), #トークン取得
+    # 新規登録処理'
+    path('signup/', views.RegisterView.as_view(), name='user-signup'),
+    # ログイン'
+    path('login/', views.LoginView.as_view(), name='user-login'),
+    #プロフィール取得
+    path('getuser/', views.GetProfileView.as_view(), name='get-userprofile'),
+    #トークン取得
+    path('gettoken/', views.GetTokenView.as_view(), name='get-token'),
 ]
